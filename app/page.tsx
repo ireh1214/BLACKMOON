dfb95e0d404daacd0b63ce7d58bbd74278012afb
ireh <ireh1214@gmail.com>
2025-08-14 16:20:14 +0900

    dd

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,40 @@
 
 import { useRouter } from 'next/navigation'
 
+const words = ['a', 'tiger', 'who', 'ate', 'the', 'moonlight']
+
 export default function Home() {
   const router = useRouter()
 
   return (
-    <div className='main_wrap'>
+    <div className="main_wrap">
+      <h2 className="animated_title">
+        {words.map((word, i) => {
+          let className = 'word'
+          if (word === 'tiger') className += ' tiger'
+          if (word === 'moonlight') className += ' moonlight'
 
-      <h2>
-        a tiger who ate the moonlight
-        </h2>
-<div className='main_img'>
+          return (
+            <span
+              key={i}
+              className={className}
+              style={{ animationDelay: `${i * 0.5}s` }}
+            >
+              {word}
+            </span>
+          )
+        })}
+      </h2>
+
+      <div className="main_img">
         <img
-        src="../img/main.png"
-        alt="메인 이미지"
-        style={{ cursor: 'pointer' }}
-        onClick={() => router.push('/p1')}
-      />
-</div>
+          src="/img/main.png"
+          alt="메인 이미지"
+          style={{ cursor: 'pointer' }}
+          onClick={() => router.push('/p1')}
+              className="main_fadein"
+        />
+      </div>
     </div>
   )
 }
